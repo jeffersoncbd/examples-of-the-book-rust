d55e1d1b2d006e8fcf8afefe70c21834db273bd3
Jefferson Carlos <jeff.carlosbd@gmail.com>
2022-12-16 17:10:40 -0300

    refatora projeto utilizando os conhecimentos do capitulo 6

--- receipt_issuer/src/main.rs
+++ receipt_issuer/src/main.rs
@@ -11,11 +11,14 @@ use crossterm::{
 struct Seller {
   name: String,
   address: String,
-  invoicing: f64
+  invoicing: f64,
 }
 impl Seller {
-  fn add_sale(&mut self, invoice: &Invoice) {
-    self.invoicing += invoice.total()
+  fn new(name: String, address: String) -> Seller {
+    Seller { name, address, invoicing: 0.0 }
+  }
+  fn new_sale(&mut self, sale: &Sale) {
+    self.invoicing += sale.total();
   }
 }
 struct Client {
@@ -26,18 +29,16 @@ struct Product {
   description: String,
   value: f64
 }
-struct Invoice {
-  seller: Seller,
+struct Sale {
   client: Client,
   product: Product,
-  amount_of_product: u32,
+  amount: u32
 }
-impl Invoice {
+impl Sale {
   fn total(&self) -> f64 {
-    // 5 * 5.24
-    let decimal_amount = Decimal::from_f64(self.amount_of_product as f64).unwrap();
-    let value_amount = Decimal::from_f64(self.product.value).unwrap();
-    let total = decimal_amount * value_amount;
+    let decimal_amount = Decimal::from_f64(self.amount as f64).unwrap();
+    let decimal_value = Decimal::from_f64(self.product.value).unwrap();
+    let total = decimal_amount * decimal_value;
     total.to_f64().unwrap()
   }
 }
@@ -52,38 +53,30 @@ impl Terminal {
     let stdout = stdout();
     let terminal_size = match terminal::size() {
       Ok(size) => size,
-      Err(_) => panic!(""),
+      Err(_) => panic!("Erro ao tentar pegar o tamanho do terminal"),
     };
     Terminal { stdout, terminal_size, end: false }
   }
-  fn print(&mut self, content: &str) -> Result<()> {
-    execute!(
-      self.stdout,
-      Print(content),
-    )?;
-    Ok(())
+  fn print(&mut self, content: &str) {
+    execute!(self.stdout, Print(content)).expect("Não foi possível imprimir");
   }
-  fn clear(&mut self) -> Result<()> {
+  fn clear(&mut self) {
     execute!(
       self.stdout,
       terminal::Clear(terminal::ClearType::All),
       cursor::MoveTo(0,0)
-    )?;
-    Ok(())
+    ).expect("Não foi possível limpar o terminal");
   }
-  fn move_to(&mut self, x: u16, y: u16) -> Result<()> {
-    execute!(
-      self.stdout,
-      cursor::MoveTo(x, y)
-    )?;
-    Ok(())
+  fn move_to(&mut self, x: u16, y: u16) {
+    execute!(self.stdout, cursor::MoveTo(x, y))
+      .expect("Não foi possível mover o cursor");
   }
-  fn fake_loading(&mut self) -> Result<()> {
+  fn fake_loading(&mut self) {
     execute!(
       self.stdout,
       terminal::Clear(terminal::ClearType::All),
       cursor::MoveTo(0, self.terminal_size.1 / 2),
-    )?;
+    ).expect("Não foi possível limpar o terminal e mover o cursos para exibir a ProgressBar");
 
     let count = 100;
     let mut pb = ProgressBar::new(count);
@@ -95,61 +88,60 @@ impl Terminal {
       thread::sleep(Duration::from_millis(1));
     }
 
-    self.clear()?;
-
-    Ok(())
+    self.clear();
   }
 }
 
-fn main() -> Result<()> {
+fn main() {
   let mut terminal = Terminal::new();
 
-  terminal.fake_loading()?;
+  terminal.fake_loading();
 
-  let mut seller = define_seller(&mut terminal)?;
+  let mut seller = define_seller(&mut terminal);
 
-  terminal.fake_loading()?;
+  terminal.fake_loading();
 
   loop {
-    application(&mut terminal, &mut seller)?;
+    application(&mut terminal, &mut seller);
     if terminal.end {
       break
     }
   }
 
-  terminal.clear()?;
-  Ok(())
+  terminal.clear();
 }
 
-fn application(terminal: &mut Terminal, seller: &mut Seller) -> Result<()> {
-  let option = menu(terminal)?;
+fn application(terminal: &mut Terminal, seller: &mut Seller) {
+  let option = menu(terminal);
   match option {
-    1 => new_sale(terminal, seller)?,
-    2 => print_billing(terminal, seller)?,
+    1 => new_sale(terminal, seller),
+    2 => print_billing(terminal, seller),
     0 => { terminal.end = true; },
     _ => panic!("Unexpected option"),
   }
-  Ok(())
 }
 
-fn read_line() -> Result<String> {
-  let mut line = String::new();
-  while let Event::Key(KeyEvent { code, .. }) = event::read()? {
-    match code {
-      KeyCode::Enter => {
-        break;
+fn read_line() -> String {
+  fn read() -> Result<String> {
+    let mut line = String::new();
+    while let Event::Key(KeyEvent { code, .. }) = event::read()? {
+      match code {
+        KeyCode::Enter => {
+          break;
+        }
+        KeyCode::Char(c) => {
+          line.push(c);
+        }
+        _ => {}
       }
-      KeyCode::Char(c) => {
-        line.push(c);
-      }
-      _ => {}
     }
+    Ok(line)
   }
-  Ok(line)
+  read().expect("Não foi possível ler a entrada do usuário")
 }
 
-fn define_seller(terminal: &mut Terminal) -> Result<Seller> {
-  terminal.clear()?;
+fn define_seller(terminal: &mut Terminal) -> Seller {
+  terminal.clear();
   execute!(
     terminal.stdout,
     Print("\n\n"),
@@ -164,25 +156,25 @@ fn define_seller(terminal: &mut Terminal) -> Result<Seller> {
     cursor::MoveTo(10, 6),
     cursor::Show,
     cursor::EnableBlinking,
-  )?;
+  ).expect("Não foi possível imprimir o formulário de cadastro do vendedor");
 
-  let name = read_line()?;
-  terminal.move_to(14, 7)?;
-  let address = read_line()?;
+  let name = read_line();
+  terminal.move_to(14, 7);
+  let address = read_line();
 
-  terminal.move_to(4, 10)?;
+  terminal.move_to(4, 10);
   println!("Se estes dados estiverem corretos, digite \"sim\":");
-  terminal.move_to(4, 11)?;
-  let confirmation = read_line()?;
+  terminal.move_to(4, 11);
+  let confirmation = read_line();
   if confirmation != String::from("sim") {
     return define_seller(terminal);
   }
 
-  Ok(Seller { name, address, invoicing: 0.0 })
+  Seller::new(name, address)
 }
 
-fn menu(terminal: &mut Terminal) -> Result<u32> {
-  terminal.clear()?;
+fn menu(terminal: &mut Terminal) -> u32 {
+  terminal.clear();
   execute!(
     terminal.stdout,
     Print("\n\n"),
@@ -198,9 +190,9 @@ fn menu(terminal: &mut Terminal) -> Result<u32> {
     cursor::MoveTo(38, 4),
     cursor::Show,
     cursor::EnableBlinking,
-  )?;
+  ).expect("Não foi possível imprimir o formulário do MENU");
 
-  let option = read_line()?;
+  let option = read_line();
 
   let option: u32 = match option.trim().parse() {
     Ok(num) => num,
@@ -210,11 +202,11 @@ fn menu(terminal: &mut Terminal) -> Result<u32> {
   if option > 2 {
     return menu(terminal)
   }
-  Ok(option)
+  option
 }
 
-fn new_sale(terminal: &mut Terminal, seller: &mut Seller) -> Result<()> {
-  terminal.clear()?;
+fn new_sale(terminal: &mut Terminal, seller: &mut Seller) {
+  terminal.clear();
   execute!(
     terminal.stdout,
     Print("\n\n"),
@@ -228,98 +220,91 @@ fn new_sale(terminal: &mut Terminal, seller: &mut Seller) -> Result<()> {
     Print("  │  Endereço:                                        │\n"),
     Print("  │                                                   │\n"),
     Print("  │  [Itens]                                          │\n"),
-    Print("  │  Descrição:                                       │\n"),
     Print("  │  Quantidade:                                      │\n"),
+    Print("  │  Descrição:                                       │\n"),
     Print("  │  Valor unitário:                                  │\n"),
     Print("  │                                                   │\n"),
     Print("  │  Valor TOTAL:                                     │\n"),
     Print("  │                                                   │\n"),
     Print("  ╰───────────────────────────────────────────────────╯\n"),
     cursor::EnableBlinking,
-  )?;
+  ).expect("Não foi possível imprimir o formulário de nova venda");
 
-  terminal.move_to(15, 4)?;
-  terminal.print(seller.name.trim())?;
-  terminal.move_to(15, 5)?;
-  terminal.print(seller.address.trim())?;
+  // print seller data
+  terminal.move_to(15, 4);
+  terminal.print(seller.name.trim());
+  terminal.move_to(15, 5);
+  terminal.print(seller.address.trim());
 
-  terminal.move_to(11, 8)?;
-  let name = read_line()?;
-  terminal.move_to(15, 9)?;
-  let address = read_line()?;
+  // define client data
+  terminal.move_to(11, 8);
+  let name = read_line();
+  terminal.move_to(15, 9);
+  let address = read_line();
   let client = Client { name, address };
 
-  terminal.move_to(16, 12)?;
-  let description = read_line()?;
-
-  fn get_amount(terminal: &mut Terminal) -> Result<u32> {
-    terminal.move_to(17, 13)?;
-    let amount = read_line()?;
+  fn get_amount(terminal: &mut Terminal) -> u32 {
+    terminal.move_to(17, 12);
+    let amount = read_line();
 
     let amount: u32 = match amount.trim().parse() {
       Ok(num) => num,
       Err(_) => {
-        terminal.move_to(17, 13)?;
-        terminal.print("                                     │")?;
-        return Ok(get_amount(terminal)?)
+        terminal.move_to(17, 13);
+        terminal.print("                                     │");
+        return get_amount(terminal)
       },
     };
-    Ok(amount)
+    amount
   }
-  let amount = get_amount(terminal)?;
+  // define amount
+  let amount = get_amount(terminal);
 
-  fn get_value(terminal: &mut Terminal) -> Result<f64> {
-    terminal.move_to(21, 14)?;
-    let value = read_line()?;
+  fn get_value(terminal: &mut Terminal) -> f64 {
+    terminal.move_to(21, 14);
+    let value = read_line();
 
     let value: f64 = match value.trim().parse() {
       Ok(num) => num,
       Err(_) => {
-        terminal.move_to(21, 14)?;
-        terminal.print("                                 │")?;
-        return Ok(get_value(terminal)?)
+        terminal.move_to(21, 14);
+        terminal.print("                                 │");
+        return get_value(terminal)
       },
     };
-    Ok(value)
+    value
   }
-  let value = get_value(terminal)?;
-
+  // define product
+  terminal.move_to(16, 13);
+  let description = read_line();
+  let value = get_value(terminal);
   let product = Product { description, value };
 
-  let invoice = Invoice {
-    seller: Seller {
-      name: seller.name.clone(),
-      address: seller.address.clone(),
-      invoicing: seller.invoicing.clone()
-    },
-    client,
-    product,
-    amount_of_product: amount
-  };
+  // define sale
+  let sale = Sale { client, product, amount };
 
-  seller.add_sale(&invoice);
+  seller.new_sale(&sale);
 
-  terminal.move_to(18, 16)?;
+  terminal.move_to(18, 16);
 
   terminal.print(&format!(
     "{}",
-    (Decimal::from_f64(amount as f64).unwrap() * Decimal::from_f64(value).unwrap()).to_f64().unwrap()
-  ))?;
-  terminal.move_to(5, 19)?;
+    sale.total()
+  ));
+  terminal.move_to(5, 19);
 
-  terminal.print("Venda adicionada! deseja imprimir o recibo?")?;
-  terminal.move_to(5, 20)?;
-  let command = read_line()?;
+  terminal.print("Venda adicionada! deseja imprimir o recibo?");
+  terminal.move_to(5, 20);
+  let command = read_line();
 
   if command == String::from("sim") {
-    print_receipt(terminal, &invoice)?;
+    print_receipt(terminal, &seller, &sale);
   }
 
-  Ok(())
 }
 
-fn print_billing(terminal: &mut Terminal, seller: &Seller) -> Result<()> {
-  terminal.clear()?;
+fn print_billing(terminal: &mut Terminal, seller: &Seller) {
+  terminal.clear();
   execute!(
     terminal.stdout,
     Print("\n\n"),
@@ -332,21 +317,20 @@ fn print_billing(terminal: &mut Terminal, seller: &Seller) -> Result<()> {
     Print("  ╰───────────────────────────────────────────────────╯\n"),
     Print("     Pressione enter para voltar..."),
     cursor::Hide,
-  )?;
+  ).expect("Não foi possível imprimir o formulário de faturamento");
 
-  terminal.move_to(15, 4)?;
-  terminal.print(seller.name.trim())?;
-  terminal.move_to(15, 5)?;
-  terminal.print(seller.address.trim())?;
-  terminal.move_to(20, 6)?;
-  terminal.print(&seller.invoicing.to_string())?;
+  terminal.move_to(15, 4);
+  terminal.print(seller.name.trim());
+  terminal.move_to(15, 5);
+  terminal.print(seller.address.trim());
+  terminal.move_to(20, 6);
+  terminal.print(&seller.invoicing.to_string());
 
-  read_line()?;
-  Ok(())
+  read_line();
 }
 
-fn print_receipt(terminal: &mut Terminal, invoice: &Invoice) -> Result<()> {
-  terminal.clear()?;
+fn print_receipt(terminal: &mut Terminal, seller: &Seller, sale: &Sale) {
+  terminal.clear();
   execute!(
     terminal.stdout,
     Print("\n\n"),
@@ -354,27 +338,27 @@ fn print_receipt(terminal: &mut Terminal, invoice: &Invoice) -> Result<()> {
     Print("  │                                          ╭──────╮ │\n"),
     Print("  │  Vendedor:                               │bonita│ │\n"),
     cursor::MoveTo(15,4),
-    Print(format!("{}", invoice.seller.name)),
+    Print(format!("{}", seller.name)),
     cursor::MoveTo(0,5),
     Print("  │  Endereço:                               │ essa │ │\n"),
     cursor::MoveTo(15,5),
-    Print(format!("{}", invoice.seller.address)),
+    Print(format!("{}", seller.address)),
     cursor::MoveTo(0,6),
     Print("  │                                          │ logo │ │\n"),
     Print("  │  Cliente:                                ╰──────╯ │\n"),
     cursor::MoveTo(14,7),
-    Print(format!("{}", invoice.client.name)),
+    Print(format!("{}", sale.client.name)),
     cursor::MoveTo(0,8),
     Print("  │  Endereço:                                        │\n"),
     cursor::MoveTo(15,8),
-    Print(format!("{}", invoice.client.address)),
+    Print(format!("{}", sale.client.address)),
     cursor::MoveTo(0,9),
     Print("  │                                                   │\n"),
     cursor::MoveTo(
-      39 - (invoice.total().to_string().len() as u16),
+      39 - (sale.total().to_string().len() as u16),
       9
     ),
-    Print(format!("VALOR TOTAL: {}\n", invoice.total())),
+    Print(format!("VALOR TOTAL: {}\n", sale.total())),
     Print("  │                                                   │\n"),
     Print("  │  [Itens]                                          │\n"),
     Print("  │                                                   │\n"),
@@ -382,21 +366,20 @@ fn print_receipt(terminal: &mut Terminal, invoice: &Invoice) -> Result<()> {
     cursor::MoveTo(5,13),
     Print(format!(
       "[{}] {} - {} ",
-      invoice.amount_of_product,
-      invoice.product.description,
-      invoice.product.value
+      sale.amount,
+      sale.product.description,
+      sale.product.value
     )),
     cursor::MoveTo(
-      51 - (invoice.total().to_string().len() as u16),
+      51 - (sale.total().to_string().len() as u16),
       13
     ),
-    Print(format!(" {}", invoice.total())),
+    Print(format!(" {}", sale.total())),
     cursor::MoveTo(0,14),
     Print("  │                                                   │\n"),
     Print("  ╰───────────────────────────────────────────────────╯\n"),
     Print("     Pressione enter para voltar..."),
     cursor::Hide,
-  )?;
-  read_line()?;
-  Ok(())
+  ).expect("Não foi possível imprimir o Recibo de venda");
+  read_line();
 }
